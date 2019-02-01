9f22aa34ea446a010997c3c5250b691cda027eaa
Julien He <julien.he@cognite.com>
2019-02-01 17:18:51 +0100

    test

--- src/utils.ts
+++ src/utils.ts
@@ -3,11 +3,16 @@ import _ from 'lodash';
 export default class Utils {
   //Converts an object to a query string, ignores properties with undefined/null values
   static getQueryString(obj: any) {
-    return _.reduce(obj, function(result: string, val: any, key: string) {
-      return _.isNil(val) ? result :
-            (_.isArray(val) ? result + [key, val].map(encodeURIComponent).join("=[") + "]&" :
-            result + [key, val].map(encodeURIComponent).join("=") + "&");
-    }, "").slice(0,-1);
+    return _.reduce(
+      obj,
+      function(result: string, val: any, key: string) {
+        return _.isNil(val)
+          ? result
+          : _.isArray(val)
+          ? result + [key, val].map(encodeURIComponent).join('=[') + ']&'
+          : result + [key, val].map(encodeURIComponent).join('=') + '&';
+      },
+      ''
+    ).slice(0, -1);
   }
-
 }
