39d23b102ff197441c0a7c76f1e763ecfca580d1
Ivan Polomani <ivan.polomanyi@cognite.com>
2020-10-21 17:02:32 +0200

    feat: token pass thru

--- src/components/configEditor.tsx
+++ src/components/configEditor.tsx
@@ -3,12 +3,25 @@ import { LegacyForms } from '@grafana/ui';
 import { DataSourcePluginOptionsEditorProps } from '@grafana/data';
 import { CogniteDataSourceOptions, CogniteSecureJsonData } from '../types';
 
-const { SecretFormField, FormField } = LegacyForms;
+const { SecretFormField, FormField, Switch } = LegacyForms;
 
-type Props = DataSourcePluginOptionsEditorProps<CogniteDataSourceOptions, CogniteSecureJsonData>;
+type ConfigEditorProps = DataSourcePluginOptionsEditorProps<
+  CogniteDataSourceOptions,
+  CogniteSecureJsonData
+>;
 
-export class ConfigEditor extends PureComponent<Props> {
-  onPathChange = (event: ChangeEvent<HTMLInputElement>) => {
+const apiUrlTooltip = `This is the URL used to reach the API.
+If the project is deployed on the default multi-tenant installation (most are),
+then keep the default value and do not change the URL.
+If the project is deployed on a separate custom cluster,
+then change the URL to point at the API server for that cluster.
+If unsure, leave the URL as default.`;
+
+const oAuthPassThruTooltip = `Forward the user's upstream OAuth identity to the data source
+(Their access token gets passed along).`;
+
+export class ConfigEditor extends PureComponent<ConfigEditorProps> {
+  onApiUrlChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { onOptionsChange, options } = this.props;
     onOptionsChange({
       ...options,
@@ -41,6 +54,17 @@ export class ConfigEditor extends PureComponent<Props> {
     });
   };
 
+  onOAuthPassThruChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { onOptionsChange, options } = this.props;
+    onOptionsChange({
+      ...options,
+      jsonData: {
+        ...options.jsonData,
+        oauthPassThru: event.currentTarget.checked,
+      },
+    });
+  };
+
   onResetAPIKey = () => {
     const { onOptionsChange, options } = this.props;
     onOptionsChange({
@@ -58,55 +82,69 @@ export class ConfigEditor extends PureComponent<Props> {
 
   render() {
     const { options } = this.props;
-    const { jsonData, secureJsonFields } = options;
-    const secureJsonData = options.secureJsonData || {};
-    const tooltip = `This is the URL used to reach the API.
-      If the project is deployed on the default multi-tenant installation (most are),
-      then keep the default value and do not change the URL.
-      If the project is deployed on a separate custom cluster,
-      then change the URL to point at the API server for that cluster.
-      If unsure, leave the URL as default.`;
+    const { secureJsonData = {}, jsonData, secureJsonFields } = options;
+    const { apiKey = '' } = secureJsonData;
+    const { cogniteProject = '', cogniteApiUrl = '', oauthPassThru } = jsonData;
 
     return (
-      <div className="gf-form-group">
-        <div className="gf-form">
-          <FormField
-            label="Project"
-            labelWidth={6}
-            inputWidth={20}
-            onChange={this.onProjectChange}
-            value={jsonData.cogniteProject || ''}
-            placeholder="Cognite Data Fusion project"
-          />
-        </div>
-
-        <div className="gf-form-inline">
-          <div className="gf-form">
-            <SecretFormField
-              isConfigured={secureJsonFields?.cogniteDataPlatformApiKey as boolean}
-              value={secureJsonData.cogniteDataPlatformApiKey || ''}
-              label="API Key"
-              placeholder="Cognite Data Fusion API key"
+      <>
+        <div className="gf-form-group">
+          <h3 className="page-heading">HTTP</h3>
+          <div className="gf-form-inline">
+            <FormField
+              label="Project"
               labelWidth={6}
               inputWidth={20}
-              onReset={this.onResetAPIKey}
-              onChange={this.onAPIKeyChange}
+              onChange={this.onProjectChange}
+              value={cogniteProject}
+              placeholder="Cognite Data Fusion project"
+              tooltip="Cognite Data Fusion project name."
+            />
+          </div>
+
+          <div className="gf-form-inline">
+            <FormField
+              label="API URL"
+              labelWidth={6}
+              inputWidth={20}
+              onChange={this.onApiUrlChange}
+              value={cogniteApiUrl}
+              placeholder="api.cognitedata.com"
+              tooltip={apiUrlTooltip}
             />
           </div>
         </div>
 
-        <div className="gf-form">
-          <FormField
-            label="Path"
-            labelWidth={6}
-            inputWidth={20}
-            onChange={this.onPathChange}
-            value={jsonData.cogniteApiUrl || ''}
-            placeholder="api.cognitedata.com"
-            tooltip={tooltip}
-          />
+        <div className="gf-form-group">
+          <h3 className="page-heading">Auth</h3>
+          <div className="gf-form-inline">
+            <Switch
+              label="Forward OAuth Identity"
+              labelClass="width-13"
+              checked={oauthPassThru}
+              onChange={this.onOAuthPassThruChange}
+              tooltip={oAuthPassThruTooltip}
+            />
+          </div>
+          {!oauthPassThru && (
+            <div className="gf-form-inline">
+              <div className="gf-form">
+                <SecretFormField
+                  isConfigured={secureJsonFields.apiKey}
+                  value={apiKey}
+                  label="API Key"
+                  placeholder="Cognite Data Fusion API key"
+                  tooltip="Cognite Data Fusion API key."
+                  labelWidth={6}
+                  inputWidth={20}
+                  onReset={this.onResetAPIKey}
+                  onChange={this.onAPIKeyChange}
+                />
+              </div>
+            </div>
+          )}
         </div>
-      </div>
+      </>
     );
   }
 }
