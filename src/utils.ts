fbefb180fe4f6a9db6ba3bff2d545886de85d1d5
Zsolt Kiss <99021303+KissSpinobleZsolt@users.noreply.github.com>
2022-02-11 10:17:49 +0200

    added Template utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,7 @@
-import { isNil, omitBy, get } from 'lodash';
+import { isNil, omitBy, get, map, split } from 'lodash';
 import { stringify } from 'query-string';
 import ms from 'ms';
+import { MutableDataFrame, FieldType } from '@grafana/data';
 import { QueryOptions, QueryTarget } from './types';
 import { FilterTypes, ParsedFilter } from './parser/types';
 
@@ -40,3 +41,67 @@ export const checkFilter = <T>(obj: T, { path, filter, value }: ParsedFilter): b
       return false;
   }
 };
+
+// Template
+export const getDataPathArray = (dataPathString: string): string[] => {
+  const dataPathArray: string[] = [];
+  map(split(dataPathString, ','), (dataPath) => {
+    const trimmed = dataPath.trim();
+    if (trimmed) {
+      dataPathArray.push(trimmed);
+    }
+  });
+  if (!dataPathArray) {
+    throw 'data path is empty!';
+  }
+  return dataPathArray;
+};
+
+export const createDatapointsDataFrame = (name: string, refId: string) => {
+  return new MutableDataFrame({
+    name,
+    refId,
+    fields: [
+      {
+        name: 'timestamp',
+        type: FieldType.time,
+      },
+      {
+        name: 'value',
+        type: FieldType.number,
+        config: {
+          displayName: name,
+        },
+      },
+    ],
+  });
+};
+
+export const getDocs = (resultsData: any, dataPath: string): any[] => {
+  if (!resultsData) {
+    throw 'resultsData was null or undefined';
+  }
+
+  const data = dataPath.split('.').reduce((d: any, p: any) => {
+    if (!d) {
+      return null;
+    }
+    return d[p];
+  }, resultsData.data);
+  if (!data) {
+    const { errors } = resultsData;
+    if (errors && errors.length !== 0) {
+      throw errors[0];
+    }
+    throw `Your data path did not exist! dataPath: ${dataPath}`;
+  }
+  if (resultsData.errors) {
+    // There can still be errors even if there is data
+    console.log('Got GraphQL errors:');
+    console.log(resultsData.errors);
+  }
+
+  return data;
+};
+
+// till here
