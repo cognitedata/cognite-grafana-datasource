5ce168c984afa0a7c99dfc2af8c27a808d983f98
Zsolt Kiss <99021303+KissSpinobleZsolt@users.noreply.github.com>
2022-03-17 13:22:49 +0200

    atomized and updated relationshipsTab

--- src/components/RelationshipsListTab.tsx
+++ src/components/RelationshipsListTab.tsx
@@ -1,74 +1,94 @@
 import { MultiSelect } from '@grafana/ui';
 import React, { useState, useEffect } from 'react';
-import { RelationshipSelectableValue, RelationshipsQuerySelector } from '../types';
+import { get, set } from 'lodash';
+import { SelectableValue } from '@grafana/data';
+import { RelationshipsSelectableValue, RelationshipsQuery } from '../types';
 import CogniteDatasource from '../datasource';
 import { SelectedProps } from './queryEditor';
+
+const relationshipQueryToSelectable = (
+  relationsShipsQuery: RelationshipsQuery[],
+  identifier: string
+): SelectableValue[] => {
+  return relationsShipsQuery.map((value) => ({
+    value: value[identifier],
+    text: value[identifier],
+  }));
+};
+
+const selectedToQuery = (
+  identifier: string,
+  values: RelationshipsSelectableValue[]
+): RelationshipsQuery[] => values.map(({ value }) => ({ [identifier]: value }));
+
+const MultiSelectField = ({
+  name,
+  type,
+  selector,
+  datasource,
+  relationsShipsQuery,
+  onQueryChange,
+  identifier,
+  refId,
+}) => {
+  const [options, setOptions] = useState({ [selector]: [] });
+  useEffect(() => {
+    datasource.getRelationshipsDropdownOptions(type, selector, refId).then(setOptions);
+  }, []);
+  const handleChange = (values) => {
+    onQueryChange({
+      relationsShipsQuery: {
+        ...set(relationsShipsQuery, selector, selectedToQuery(identifier, values)),
+      },
+    });
+  };
+  return (
+    <MultiSelect
+      options={options[selector]}
+      value={relationshipQueryToSelectable(get(relationsShipsQuery, selector), identifier)}
+      allowCustomValue
+      onChange={handleChange}
+      className="cognite-dropdown"
+      placeholder={`Filter relations by ${name}`}
+      maxMenuHeight={150}
+    />
+  );
+};
 
 export const RelationshipsListTab = (props: SelectedProps & { datasource: CogniteDatasource }) => {
   const { query, datasource, onQueryChange } = props;
   const { relationsShipsQuery } = query;
-  const [options, setOptions] = useState<RelationshipsQuerySelector>({
-    dataSetIds: [],
-    labels: {
-      containsAll: [],
+  const fieldSettings = [
+    {
+      type: 'datasets',
+      name: 'Dataset',
+      selector: 'dataSetIds',
+      identifier: 'id',
     },
-  });
-  const [selectedOptions, setSelectedOptions] = useState<RelationshipsQuerySelector>({
-    dataSetIds: [],
-    labels: {
-      containsAll: [],
+    {
+      type: 'labels',
+      name: 'Label',
+      selector: 'labels.containsAll',
+      identifier: 'externalId',
     },
-  });
-  const handleChange = (values: RelationshipSelectableValue[], target: string) => {
-    setSelectedOptions({ ...selectedOptions, [target]: values });
-    if (target === 'labels') {
-      onQueryChange({
-        relationsShipsQuery: {
-          ...relationsShipsQuery,
-          labels: {
-            containsAll: values.map(({ value }) => ({ externalId: value })),
-          },
-        },
-      });
-    } else {
-      onQueryChange({
-        relationsShipsQuery: {
-          ...relationsShipsQuery,
-          datasets: values.map(({ value }) => ({ id: value })),
-        },
-      });
-    }
-  };
-  const getDropdowns = async () => {
-    const { labels, dataSetIds } = await datasource.getRelationshipsDropdowns(query.refId);
-    setOptions({
-      dataSetIds,
-      labels,
-    });
-  };
-  useEffect(() => {
-    getDropdowns();
-  }, []);
+  ];
   return (
     <div className="full-width-row">
-      <MultiSelect
-        options={options.dataSetIds}
-        value={selectedOptions.dataSetIds}
-        allowCustomValue
-        onChange={(value) => handleChange(value, 'dataSetIds')}
-        className="cognite-dropdown"
-        placeholder="Filter relations by dataset"
-        maxMenuHeight={150}
-      />
-      <MultiSelect
-        options={options.labels.containsAll}
-        value={selectedOptions.labels.containsAll}
-        allowCustomValue
-        onChange={(value) => handleChange(value, 'labels')}
-        className="cognite-dropdown"
-        placeholder="Filter relations by Label"
-        maxMenuHeight={150}
-      />
+      {fieldSettings.map(({ type, name, selector, identifier }) => (
+        <MultiSelectField
+          {...{
+            name,
+            type,
+            selector,
+            datasource,
+            relationsShipsQuery,
+            onQueryChange,
+            identifier,
+            refId: query.refId,
+          }}
+          key={type}
+        />
+      ))}
     </div>
   );
 };
