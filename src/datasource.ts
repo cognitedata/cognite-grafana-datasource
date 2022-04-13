3caff5d5a045234318191766495dca97226ff0dc
Zsolt Kiss <99021303+KissSpinobleZsolt@users.noreply.github.com>
2022-04-13 15:30:01 +0300

    removed extra case

--- src/datasource.ts
+++ src/datasource.ts
@@ -643,9 +643,6 @@ export async function getDataQueryRequestItems(
       items = await formQueriesForExpression(expr, target, connector, defaultInterval);
       break;
     }
-    case Tab.Relationships: {
-      break;
-    }
   }
   return { type, items, target };
 }
