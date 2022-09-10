379a5277d99f331c4aaac77325079ee136b230c8
Zsolt Kiss <99021303+KissSpinobleZsolt@users.noreply.github.com>
2022-09-10 11:09:04 +0300

    add aggregations component

--- src/components/advancedEventFilter.tsx
+++ src/components/advancedEventFilter.tsx
@@ -1,8 +1,93 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { Button, CodeEditor, Tooltip } from '@grafana/ui';
+import { Button, CodeEditor, Segment, Switch, Tooltip, InlineFormLabel } from '@grafana/ui';
 import jsonlint from 'jsonlint-mod';
 import { EventQuery } from '../types';
 import { EventAdvancedFilterHelp } from './queryHelp';
+import { InlineButton, SelectedProps } from './queryEditor';
+
+const ActiveAggregateCheckbox = ({ query, onQueryChange }: SelectedProps) => {
+  return (
+    <div className="gf-form gf-form-inline">
+      <InlineFormLabel tooltip="Fetch with Aggregate count " width={10}>
+        With Aggregate
+      </InlineFormLabel>
+      <div className="gf-form-switch">
+        <Switch
+          value={query.eventQuery.withAggregate}
+          onChange={({ currentTarget }) =>
+            onQueryChange({
+              eventQuery: {
+                ...query.eventQuery,
+                withAggregate: currentTarget.checked,
+              },
+            })
+          }
+        />
+      </div>
+    </div>
+  );
+};
+const FieldsTypeColumnsPicker = ({ query, onQueryChange }: SelectedProps) => {
+  const options = [];
+  useEffect(() => {
+    if (!query.eventQuery.property?.length) {
+      onQueryChange({
+        eventQuery: {
+          ...query.eventQuery,
+          property: [],
+        },
+      });
+    }
+  }, []);
+  const property = query.eventQuery.property || [];
+  const onEventQueryChange = (e: Partial<EventQuery>) => {
+    onQueryChange({
+      eventQuery: {
+        ...query.eventQuery,
+        ...e,
+      },
+    });
+  };
+  return (
+    <div className="gf-form">
+      <InlineFormLabel tooltip="Choose which fields properties" width={10}>
+        Field properties
+      </InlineFormLabel>
+      <div className="gf-form" style={{ flexWrap: 'wrap' }}>
+        {property.map((val, key) => (
+          <>
+            <Segment
+              value={val}
+              options={options}
+              onChange={({ value }) => {
+                onEventQueryChange({
+                  property: property.map((old, i) => (i === key ? value : old)),
+                });
+              }}
+              allowCustomValue
+            />
+            <InlineButton
+              onClick={() => {
+                onEventQueryChange({
+                  property: property.filter((_, i) => i !== key),
+                });
+              }}
+              iconName="times"
+            />
+          </>
+        ))}
+        <InlineButton
+          onClick={() => {
+            onEventQueryChange({
+              property: [...property, 'type'],
+            });
+          }}
+          iconName="plus-circle"
+        />
+      </div>
+    </div>
+  );
+};
 
 export const AdvancedEventFilter = (props) => {
   const { query, onQueryChange } = props;
@@ -50,6 +135,8 @@ export const AdvancedEventFilter = (props) => {
           onSave={onChange}
         />
       </div>
+      <ActiveAggregateCheckbox {...{ query, onQueryChange }} />
+      {query.eventQuery.withAggregate && <FieldsTypeColumnsPicker {...{ query, onQueryChange }} />}
       {showHelp && <EventAdvancedFilterHelp onDismiss={() => setShowHelp(false)} />}
     </>
   );
