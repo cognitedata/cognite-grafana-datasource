d26a336a1bcc13203e0d96d9af68acb75dc3785c
Zsolt Kiss <99021303+KissSpinobleZsolt@users.noreply.github.com>
2022-09-16 13:59:12 +0300

    to table data

--- src/datasources/ExtractionPipelineDatasource.ts
+++ src/datasources/ExtractionPipelineDatasource.ts
@@ -1,7 +1,9 @@
 import { DataQueryRequest, DataQueryResponse } from '@grafana/data';
+import _ from 'lodash';
 import { CogniteQuery, ExtractionPipelineQuery, HttpMethod } from '../types';
 import { Connector } from '../connector';
 import { handleError } from '../datasource';
+import { convertItemsToTable } from '../cdf/client';
 
 export class ExtractionPipelineDatasource {
   public constructor(private connector: Connector) {}
@@ -38,11 +40,27 @@ export class ExtractionPipelineDatasource {
           return [];
         }
         if (selection.length > 1) {
+          const columns = ['index'];
+          const epData = [];
           const items = await Promise.all(
             selection.map(({ value }) => this.fetchExtractionPipelinesRuns({ externalId: value }))
           );
-          console.log(items);
-          return items;
+          _.map(items, (item, index) => {
+            const fixNodeProps = { index };
+            _.mapValues(item, (value, key) => {
+              if (_.isObject(value)) {
+                _.mapKeys(value, (_, objKey) => columns.push(objKey));
+                epData.push({
+                  index,
+                  key,
+                  ...value,
+                });
+              }
+              columns.push(!_.isObject(value) ? key : 'key');
+              _.assignIn(fixNodeProps, !_.isObject(value) && { [key]: value });
+            });
+          });
+          return convertItemsToTable(epData, _.uniq(columns));
         }
         const items = await this.connector.fetchItems({
           path: '/extpipes/runs/list',
