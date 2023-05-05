ff4a4dc115c33cae09b2de10c5dd273c41b90e7d
David Liu <david.liu@cognite.com>
2023-05-05 14:39:41 +0200

    test

---
+++ src/datasources/FlexibleDataModelingDatasource.spec.ts
@@ -0,0 +1,95 @@
+import { FlexibleDataModellingDatasource } from './FlexibleDataModellingDatasource';
+import { Connector } from '../connector';
+import { TimeseriesDatasource } from './TimeseriesDatasource';
+
+describe('FlexibleDataModellingDatasource', () => {
+  let datasource: FlexibleDataModellingDatasource;
+  const connectorMock = {} as Connector;
+  const timeseriesDatasourceMock = {} as TimeseriesDatasource;
+
+  beforeEach(() => {
+    datasource = new FlexibleDataModellingDatasource(connectorMock, timeseriesDatasourceMock);
+  });
+
+  describe('listFlexibleDataModelling', () => {
+    it('should return data when successful', async () => {
+      // Mock the fetchQuery method of the connector to return a successful response
+      connectorMock.fetchQuery = jest.fn().mockResolvedValue({
+        data: {
+          listGraphQlDmlVersions: {
+            items: [
+              {
+                space: 'test',
+                externalId: 'test',
+                version: '1',
+                name: 'test',
+                description: 'test',
+                graphQlDml: 'test',
+                createdTime: 'test',
+                lastUpdatedTime: 'test',
+              },
+            ],
+          },
+        },
+      });
+
+      const result = await datasource.listFlexibleDataModelling('test-ref-id');
+
+      expect(result.listGraphQlDmlVersions.items).toHaveLength(1);
+    });
+
+    it('should return an empty list when an error occurs', async () => {
+      // Mock the fetchQuery method of the connector to throw an error
+      connectorMock.fetchQuery = jest.fn().mockRejectedValue(new Error('Test error'));
+
+      const result = await datasource.listFlexibleDataModelling('test-ref-id');
+
+      expect(result.listGraphQlDmlVersions.items).toHaveLength(0);
+    });
+  });
+
+  describe('listVersionByExternalIdAndSpace', () => {
+    it('should return data when successful', async () => {
+      // Mock the fetchQuery method of the connector to return a successful response
+      connectorMock.fetchQuery = jest.fn().mockResolvedValue({
+        data: {
+          graphQlDmlVersionsById: {
+            items: [
+              {
+                space: 'test',
+                externalId: 'test',
+                version: '1',
+                name: 'test',
+                description: 'test',
+                graphQlDml: 'test',
+                createdTime: 'test',
+                lastUpdatedTime: 'test',
+              },
+            ],
+          },
+        },
+      });
+
+      const result = await datasource.listVersionByExternalIdAndSpace(
+        'test-ref-id',
+        'test',
+        'test'
+      );
+
+      expect(result.graphQlDmlVersionsById.items).toHaveLength(1);
+    });
+
+    it('should return an empty list when an error occurs', async () => {
+      // Mock the fetchQuery method of the connector to throw an error
+      connectorMock.fetchQuery = jest.fn().mockRejectedValue(new Error('Test error'));
+
+      const result = await datasource.listVersionByExternalIdAndSpace(
+        'test-ref-id',
+        'test',
+        'test'
+      );
+
+      expect(result.graphQlDmlVersionsById.items).toHaveLength(0);
+    });
+  });
+});
