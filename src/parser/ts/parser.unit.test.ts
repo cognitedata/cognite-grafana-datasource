fe30870b51d19ddd2cf1a2a4b05a49ad1c130046
abdullah-cognite <100700755+abdullah-cognite@users.noreply.github.com>
2024-05-06 15:05:07 +0200

    update parser comment

--- src/parser/ts/parser.unit.test.ts
+++ src/parser/ts/parser.unit.test.ts
@@ -542,7 +542,8 @@ describe('parse unary "-" operator', () => {
       const startTime = Date.now(); // get current time
       expect(parse(query)).toEqual(expected);
       const endTime = Date.now(); // get current time
-      const timeDiff = endTime - startTime; // in ms
+      const timeDiff = endTime - startTime; // in 
+      // in case you have to bump the limit up from 10ms, make sure you know what you are doing :)
       expect(timeDiff).toBeLessThan(10); // expect test to pass in 1 second
     },1000)
   );
