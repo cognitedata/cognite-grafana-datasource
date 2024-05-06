409ad043f95668122b420c64581ac8b497676f16
abdullah-cognite <100700755+abdullah-cognite@users.noreply.github.com>
2024-05-06 15:15:12 +0200

    remove timeout from test

--- src/parser/ts/parser.unit.test.ts
+++ src/parser/ts/parser.unit.test.ts
@@ -545,7 +545,7 @@ describe('parse unary "-" operator', () => {
       const timeDiff = endTime - startTime; 
       // in case you have to bump the limit up from 10ms, make sure you know what you are doing :)
       expect(timeDiff).toBeLessThan(10); // expect test to pass in 10ms
-    },1000)
+    })
   );
 
   
