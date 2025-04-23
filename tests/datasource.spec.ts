908186301e6a4a22e8b3397d7b67164d6e41b0a5
Ivan Polomani <ivan.polomanyi@cognite.com>
2025-04-23 14:12:40 +0200

    test: relationships panel

--- tests/datasource.spec.ts
+++ tests/datasource.spec.ts
@@ -24,3 +24,27 @@ test('Panel with multiple time series queries rendered OK', async ({ gotoDashboa
     await expect(button).toBeVisible();
   }
 });
+
+
+test('Panel with Relationships rendered OK', async ({ gotoDashboardPage, readProvisionedDashboard, page }) => {
+  const dashboard = await readProvisionedDashboard({ fileName: 'weather-station.json' });
+  const dashboardPage = await gotoDashboardPage(dashboard);
+
+  var expectedElements = [
+    '59.9139-10.7522-current.clouds',
+    '59.9139-10.7522-current.feels_like',
+    '59.9139-10.7522-current.humidity',
+    '59.9139-10.7522-current.pressure',
+    '59.9139-10.7522-current.temp',
+    '59.9139-10.7522-current.wind_speed',
+    '59.9139-10.7522-current.uvi',
+    'Oslo',
+    'Oslo asset',
+  ].sort();
+
+  await expect.poll(async () => {
+      const panel1 = await dashboardPage.getPanelByTitle('Relationships');
+      const nodes = await panel1.locator.getByLabel(/Node/).getByText(/.+/).allInnerTexts();
+      return nodes.sort();
+    }, { timeout: 1000 }).toEqual(expectedElements);
+});
