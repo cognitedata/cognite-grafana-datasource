b076a61c65a546ceea7dc7ab7ee0477d4c94664a
Ivan Polomani <ivan.polomanyi@cognite.com>
2025-04-25 15:53:21 +0200

    test: add annotation editor e2e test

---
+++ tests/annotationQueryEditor.spec.ts
@@ -0,0 +1,18 @@
+import semver from 'semver';
+import { test as base, expect, PluginFixture, PluginOptions } from '@grafana/plugin-e2e';
+import { readProvisionedDataSource } from '../playwright/fixtures/readProvisionedDataSource';
+const test = base.extend<PluginFixture, PluginOptions>({ readProvisionedDataSource });
+
+test('annotation query in provisioned dashboard should return a 200 response', async ({
+    readProvisionedDashboard,
+    gotoAnnotationEditPage,
+    page,
+    grafanaVersion,
+  }) => {
+    const dashboard = await readProvisionedDashboard({ fileName: 'weather-station.json' });
+    const annotationEditPage = await gotoAnnotationEditPage({ dashboard, id: '1' });
+    await await page.getByTestId('data-testid annotations-test-button').click();
+    if (semver.gte(grafanaVersion, '11.0.0')) {
+        await expect(annotationEditPage).toHaveAlert('success', { hasText: /[0-9]+ events.*/ });
+    }
+  });
