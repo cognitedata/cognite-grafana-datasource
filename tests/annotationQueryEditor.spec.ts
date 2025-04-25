7ee25b2de3b3b2ffa68f97fe0802afa97b8a6be9
Ivan Polomani <ivan.polomanyi@cognite.com>
2025-04-25 16:02:13 +0200

    fix: test

--- tests/annotationQueryEditor.spec.ts
+++ tests/annotationQueryEditor.spec.ts
@@ -11,7 +11,7 @@ test('annotation query in provisioned dashboard should return a 200 response', a
   }) => {
     const dashboard = await readProvisionedDashboard({ fileName: 'weather-station.json' });
     const annotationEditPage = await gotoAnnotationEditPage({ dashboard, id: '1' });
-    await await page.getByTestId('data-testid annotations-test-button').click();
+    await page.getByTestId('data-testid annotations-test-button').click();
     if (semver.gte(grafanaVersion, '11.0.0')) {
         await expect(annotationEditPage).toHaveAlert('success', { hasText: /[0-9]+ events.*/ });
     }
